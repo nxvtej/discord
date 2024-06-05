ee0f8b99ccd9fe5b9038a8358d192033ffa223ef
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-06-05 22:31:02 +0530

    8. added logout button and dark mode button

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,21 +1,21 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 import { cn } from "@/lib/utils"
+import { ModeToggle } from "@/components/mode-toggle";
 
 
 const state = false;
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex justify-between">
     <p className="text-3xl font-bold text-indigo-500">
       this is protexted route
-      
     </p>
 
-
-    <Button >
-      click me</Button>
+    <UserButton 
+    afterSignOutUrl="/"/>
+    <ModeToggle />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 
 
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -20,8 +21,18 @@ export default function RootLayout({
   return (
     <ClerkProvider>
 
-    <html lang="en">
-      <body className={font.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={font.className}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem={false}
+        storageKey="discord-theme"
+        >
+        {children}
+        </ThemeProvider>
+     
+        </body>
     </html>
 
     </ClerkProvider>
