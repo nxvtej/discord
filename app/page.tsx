6f670dbeeae6cf7e7ac56cb1737f8acdc9832e38
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-06-05 18:47:36 +0530

    playing with https://ui.shadcn.com/docs/ to check components

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+    <p className="text-3xl font-bold text-indigo-500">
+      this is discord clone
+    </p>
+    <Button variant="destructive"> click me</Button>
+    </div>
+  );
+}
