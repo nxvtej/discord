c7dd33a4138c63260fe49fcfded724f2f6554520
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-06-05 22:03:26 +0530

    fixed middleware

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="">
     <p className="text-3xl font-bold text-indigo-500">
-      this is protecxted route
+      this is protexted route
       
     </p>
 
