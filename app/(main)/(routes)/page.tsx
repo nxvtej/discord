14a1fa9c8a7e0449451398f0e3b70933a68402fa
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-06-05 19:52:14 +0530

    5. sign in and sign up working fine

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -9,7 +9,8 @@ export default function Home() {
   return (
     <div className="">
     <p className="text-3xl font-bold text-indigo-500">
-      this is discord clone
+      this is protecxted route
+      
     </p>
 
 
